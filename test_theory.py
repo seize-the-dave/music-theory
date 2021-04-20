5f5a127915efc91fa32f8131eca626e3487985c0
Dave Grant <daveg@backbase.com>
2021-04-20 11:27:39 +0100

    Added descending scales.

--- test_theory.py
+++ test_theory.py
@@ -5,8 +5,14 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import theory
 for scale in theory.MAJOR_SCALES:
     print(theory.major_scale(scale))
+    print(theory.major_scale(scale, False))
 
 for scale in theory.MINOR_SCALES:
     print(theory.minor_scale(scale))
+    print(theory.minor_scale(scale, False))
+    print(theory.harmonic_minor_scale(scale))
+    print(theory.harmonic_minor_scale(scale, False))
+    print(theory.melodic_minor_scale(scale))
+    print(theory.melodic_minor_scale(scale, False))
 
 # print(theory.minor_scale('B♭'))
--- theory.py
+++ theory.py
@@ -4,64 +4,90 @@ MAJOR_SCALES = ['C','G','D','A','E','B','F♯','C♯','G♯','F','B♭','E♭','
 MINOR_SCALES = ['A','E','B','F♯','C♯','G♯','D♯','A♯','E♯','D','G','C','F','B♭','E♭','A♭','D♭']
 
 NOTES = [
-        ['A','B𝄫'],
-        ['B♭','A♯'],
-        ['B','C♭'],
-        ['C','B♯'],
-        ['C♯','D♭'],
-        'D',
-        ['E♭','D♯'],
-        ['E','F♭'],
-        ['F','E♯'],
-        ['F♯','G♭'],
-        ['G','F𝄪'],
-        ['G♯','A♭']
-    ]
+    ['A','B𝄫'],  # WHITE
+    ['B♭','A♯'], # BLACK
+    ['B','C♭'],  # WHITE
+    ['C','B♯'],  # WHITE
+    ['C♯','D♭'], # BLACK
+    'D',         # WHITE
+    ['E♭','D♯'], # BLACK
+    ['E','F♭'],  # WHITE
+    ['F','E♯'],  # WHITE
+    ['F♯','G♭'], # BLACK
+    ['G','F𝄪'],  # WHITE
+    ['G♯','A♭']  # BLACK
+]
 
-def make_tonic(tonic, notes):
+def make_tonic(tonic, notes, ascending=True):
     index = None
     for i in range(len(notes)):
         if type(notes[i] == list) and tonic in notes[i]:
             index = i
         elif type(notes[i] == str) and tonic == notes[i]:
             index = i
-    return rotate(notes, index)
+    notes = rotate(notes, index)
+    # if ascending == False:
+    #     notes.reverse()
+    
+    return notes
 
 def rotate(l, n):
     return l[n:] + l[:n]
 
-def major_scale(note):
-    return extract_notes(make_tonic(note, NOTES), note, 'TTSTTTS')
+def major_scale(note, ascending=True):
+    if ascending:
+        return extract_notes(make_tonic(note, NOTES), note, 'TTSTTTS', ascending)
+    else:
+        return extract_notes(make_tonic(note, NOTES), note, 'STTTSTT', ascending)
 
-def minor_scale(note):
-    return extract_notes(make_tonic(note, NOTES), note, 'TSTTSTT')
+def minor_scale(note, ascending=True):
+    if ascending:
+        return extract_notes(make_tonic(note, NOTES), note, 'TSTTSTT', ascending)
+    else:
+        return extract_notes(make_tonic(note, NOTES), note, 'TTSTTST', ascending)
 
-def extract_notes(notes, tonic, pattern):
+def harmonic_minor_scale(note, ascending=True):
+    if ascending:
+        return extract_notes(make_tonic(note, NOTES), note, 'TSTTSAS', ascending)
+    else:
+        return extract_notes(make_tonic(note, NOTES), note, 'SASTTST', ascending)
+
+def melodic_minor_scale(note, ascending=True):
+    if ascending:
+        return extract_notes(make_tonic(note, NOTES), note, 'TSTTTTS', ascending)
+    else:
+        return extract_notes(make_tonic(note, NOTES), note, 'TSTTSTT', ascending)
+
+def extract_notes(notes, tonic, pattern, ascending = True):
     scale = [tonic]
     offset = 0
     last_letter = tonic[0]
     for step in pattern:
-        if (step == 'T'):
-            offset += 2
-        else:
-            offset += 1
-        options = notes[offset % len(notes)]
-        this_letter = LETTERS[(LETTERS.index(last_letter) + 1) % len(LETTERS)]
+        step_change = 0
 
-        found = False
+        if (step == 'T'): # Tone
+            step_change = 2
+        elif (step == 'A'): # Augmented
+            step_change = 3
+        else:
+            step_change = 1 # Semitone
+
+        if ascending:
+            offset += step_change
+            this_letter = LETTERS[(LETTERS.index(last_letter) + 1) % len(LETTERS)]
+        else:
+            offset -= step_change
+            this_letter = LETTERS[(LETTERS.index(last_letter) - 1) % len(LETTERS)]
+
+        options = notes[offset % len(notes)]
+
         if type(options) == str and options[0] == this_letter:
             scale.append(options)
             last_letter = options[0]
-            found = True
         elif type(options) == list:
             for option in options:
                 if option[0] == this_letter:
                     scale.append(option)
                     last_letter = this_letter
-                    found = True
-        if found == False:
-            print("Couldn't find note matching " + this_letter + " for " + tonic)
-            print("Progress: " + str(scale))
-            print("Was looking in " + str(options))
                 
     return scale
