a18c913e666ee47c10f8b573c8d56498df1bb39a
Dave Grant <daveg@backbase.com>
2021-04-20 09:50:05 +0100

    Added a few more major and minor keys

--- test_theory.py
+++ test_theory.py
@@ -3,10 +3,10 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import theory
-# for scale in theory.MAJOR_SCALES:
-#     print(theory.major_scale(scale))
+for scale in theory.MAJOR_SCALES:
+    print(theory.major_scale(scale))
 
-# for scale in theory.MINOR_SCALES:
-#     print(theory.minor_scale(scale))
+for scale in theory.MINOR_SCALES:
+    print(theory.minor_scale(scale))
 
-print(theory.minor_scale('B♭'))
+# print(theory.minor_scale('B♭'))
